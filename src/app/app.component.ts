c57f7948c7095ba06c20a054c1ad9c2537abcb94
HYrelav <33786647+HYrelav@users.noreply.github.com>
2019-05-18 19:46:40 +0200

    Added Validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-ex002-reactiveform';
+
+  userForm: FormGroup;
+  myPseudo: string;
+  myFirstName: string;
+  myLastName: string;
+  myEmail: string;
+  myPhone: string;
+
+  constructor(private formBuilder: FormBuilder) {};
+
+  ngOnInit() {
+    this.initForm();
+  }
+  
+  initForm() {
+    this.userForm = this.formBuilder.group({
+      pseudo: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['']
+    })
+  }
+
+  onSubmitForm() {
+    const formValue = this.userForm.value;
+    
+    this.myPseudo=formValue['pseudo'];
+    this.myFirstName=formValue['firstName'];
+    this.myLastName=formValue['lastName'];
+    this.myEmail=formValue['email'];
+    this.myPhone=formValue['phone'];
+
+
+  }
 }
